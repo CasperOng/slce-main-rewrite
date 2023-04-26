c43de5a862f482e648f4bc8e36dc47d954f5341f
Casper Ong <casper10528@gmail.com>
2023-04-26 23:50:04 +0800

    fix ipad pro responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,47 @@
-import { Layout, Text, Page } from '@vercel/examples-ui'
+// import { Layout, Text, Page } from '@vercel/examples-ui'
+// import { Chat } from '../components/Chat'
+
+// function Home() {
+//   return (
+//     <Page className="flex flex-col gap-12">
+//       <section className="flex flex-col gap-6">
+//         <Text variant="h1">Xiri</Text>
+//         <Text className="text-zinc-600">
+//           A simple chat bot is implemented using Next.js, API
+//           Routes, and OpenAI API.
+//         </Text>
+//       </section>
+
+//       <section className="flex flex-col gap-3">
+//         <Text variant="h2">Try to type something!</Text>
+//         <div className="lg:w-2/3">
+//           <Chat />
+//         </div>
+//       </section>
+//     </Page>
+//   )
+// }
+
+// Home.Layout = Layout
+
+// export default Home
+
+import { Text, Page } from '@vercel/examples-ui'
 import { Chat } from '../components/Chat'
 
 function Home() {
   return (
-    <Page className="flex flex-col gap-12">
-      <section className="flex flex-col gap-6">
-        <Text variant="h1">OpenAI GPT-3 text model usage example</Text>
+    <Page>
+      <section>
+        <Text variant="h1">Xiri</Text>
         <Text className="text-zinc-600">
-          In this example, a simple chat bot is implemented using Next.js, API
+          A simple chat bot is implemented using Next.js, API
           Routes, and OpenAI API.
         </Text>
       </section>
 
-      <section className="flex flex-col gap-3">
-        <Text variant="h2">AI Chat Bot:</Text>
+      <section>
+        <Text variant="h2">Try to type something!</Text>
         <div className="lg:w-2/3">
           <Chat />
         </div>
@@ -21,7 +49,5 @@ function Home() {
     </Page>
   )
 }
-
-Home.Layout = Layout
 
 export default Home
