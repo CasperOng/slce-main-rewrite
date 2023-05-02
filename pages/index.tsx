ae5b8fb2cbc9a718cec1d7d066f99e1a1f5cd8b2
Casper Ong NY <casper10528@gmail.com>
2023-05-02 15:58:00 +0800

    vercel
    Vercel

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ function Home() {
     <Page>
       <link rel="stylesheet" type="text/css" href="./styles.css" />
       <section>
-        <Text variant="h1">Xiri</Text>
+        <Text variant="h1">Xiri</Text> 
       </section>
 
       <section>
@@ -19,7 +19,7 @@ function Home() {
       </section>
 
       <footer>
-        <Text>Created by <a href="https://casperong.eu.org" target="_blank" rel="noopener noreferrer">Casper Ong</a></Text>
+        <Text>Created by <a href="https://casperong.eu.org" target="_blank" rel="noopener noreferrer">Casper Ong NY</a></Text>
         <Text>Hosted on <a href="https://vercel.com/" target="_blank" rel="noopener noreferrer">Vercel</a></Text>
         <Text>Licensed under the <a href="https://www.mozilla.org/en-US/MPL/2.0/" target="_blank" rel="noopener noreferrer">Mozilla Public License 2.0</a></Text>
         <Text>App icon was made by <a href="https://www.flaticon.com/authors/juicy-fish" target="_blank" rel="noopener noreferrer">juicy_fish</a></Text>
