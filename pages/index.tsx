100d8ff74e3729233837c768afcb58a4bec804f8
Casper Ong NY <casper10528@gmail.com>
2023-06-01 15:17:29 +0800

    Eng to Chi

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,14 +12,13 @@ function Home() {
       </section>
 
       <section>
-        <Text >Try to type something!</Text>
+        <Text >試著輸入一些東西！</Text>
         <div className="lg:w-2/3">
           <Chat />
         </div>
       </section>
 
       <footer>
-        <Text>Created by <a href="https://casperong.eu.org" target="_blank" rel="noopener noreferrer">Casper Ong NY</a></Text>
         <Text>Hosted on <a href="https://vercel.com/" target="_blank" rel="noopener noreferrer">Vercel</a></Text>
         <Text>Licensed under the <a href="https://www.mozilla.org/en-US/MPL/2.0/" target="_blank" rel="noopener noreferrer">Mozilla Public License 2.0</a></Text>
         <Text>App icon was made by <a href="https://www.flaticon.com/authors/juicy-fish" target="_blank" rel="noopener noreferrer">juicy_fish</a></Text>
